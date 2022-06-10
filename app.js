26add374edcaceebaddb4f8195054ad6c2558630
Diego Aguilar <DiegoAR@unisabana.edu.co>
2022-06-10 17:15:28 -0500

    cambie console

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.get('/', (req, res) => {
     console.log('peticion recibida')
-    res.status(200).send('<h1> Hola Mundo con NODEMON')
+    res.status(200).send('<h1> Hola Mundo SIN ENV')
 })
 
 const PORT= process.env.PORT || 4000
